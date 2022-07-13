c4edab162134be5a1883c45fc80a1f96b425d0b1
Rahul <rahul.connect200@gmail.com>
2022-07-13 15:40:37 +0530

    fifth commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+import {ethers} from 'ethers';
+
+
+
+function App(){
+  const [tasks,setTasks] = useState([]); 
+  const [input,setInput] = useState("");
+  const [currentAccount,setCurrentAccount] = useState("");
+  const [correctNetwork,setCorrectNetwork] = useState(false);
+
+  const connectWallet = async()=>{
+    try{
+       // Write here 
+       
+    }catch(error){
+      console.log("Error Connecting to metamask",error);
+    }
+  }
+
+  
+
+
+  useEffect(()=>{
+    connectWallet();
+  },[]);
+
+
+  return (
+    <div>
+      {currentAccount == '' ? (
+        <center><button className='button' onClick={connectWallet}>Connect Wallet</button></center>
+      ) : correctNetwork ? (
+        <div className='App'>
+          <h2>Meta mask connected Successfully !</h2>
+        </div>
+      ) : (
+        <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
+           <div>Please connect to the Goerli Testnet and reload the screen</div>
+        </div>
+      )}
+    </div>
+  );
+  
+
+}
+
+export default App;
